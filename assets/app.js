01b896a1b376f4e3f2cce17380eb7d437e924ed4
CaritoDasilva <caritodasilva@gmail.com>
2018-08-06 15:26:43 -0400

    se agrega estilo a la primera tarjeta del dom

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 plusWork = document.getElementById('homeworkDiv');
 
 function drawCard() {
-  homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;"> <div class = "card-body"><h5 class = "card-title" > Card title < /h5> <p class = "card-text" > Some quick example text to build on the card title and make up the bulk of the card 's content.</p> <a href = "#" class = "btn btn-primary"> Go somewhere</a></div></div>`;
+  homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;"> <div class = "card-body"><h5 class = "card-title" > <input class="form-control form-control-lg plusWork" type="text" placeholder="Introduzca el título de la lista..." id="plusWorkBefore"> <a href = "#" class = "btn btn-primary" id="listBtn"> Añadir Lista</a></div></div>`;
 
 }
