f03a4e86e24a519a293d63ed8d54fb9368c85fbe
CaritoDasilva <caritodasilva@gmail.com>
2018-08-06 15:44:07 -0400

    se agregan tareas

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,10 @@
-plusWork = document.getElementById('homeworkDiv');
+listBtn = null;
 
 function drawCard() {
-  homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;"> <div class = "card-body"><h5 class = "card-title" > <input class="form-control form-control-lg plusWork" type="text" placeholder="Introduzca el título de la lista..." id="plusWorkBefore"> <a href = "#" class = "btn btn-primary" id="listBtn"> Añadir Lista</a></div></div>`;
+  homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;"> <div class = "card-body"><h5 class = "card-title" > <input class="form-control form-control-lg plusWork" type="text" placeholder="Introduzca el título de la lista..." id="plusWorkBefore"> <a href = "#" class = "btn btn-primary" id="listBtn" onclick="drawList()"> Añadir Lista</a></div></div>`;
 
 }
+
+function drawList() {
+  homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;"> <div class = "card-body"><h5 class = "card-title">${plusWorkBefore.value}</h5>`;
+};
