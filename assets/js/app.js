994e968a058bef7f83fb42faa5258c88ac52ab1d
CaritoDasilva <caritodasilva@gmail.com>
2018-08-08 17:58:07 -0400

    escribiendo funciones para sacar la info de listas y tareas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 let listBtn = null;
 let nameList = [];
 let titleList;
+let cardList = [];
+let listingCards = '';
 
 function drawCard() {
   homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;"> <div class = "card-body"><h5 class = "card-title" > <input class="form-control form-control-lg plusWork" type="text" placeholder="Introduzca el título de la lista..." id="plusWorkBefore"> <a href = "#" class = "btn btn-primary listBtn" onclick="drawList();saveNameList()"> Añadir Lista</a></div></div>`;
@@ -16,7 +18,29 @@ function drawList() {
 
 function addOneHomework() {
   homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-    <h5 class="card-title">${titleList}</h5>
-    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Introduzca un título para esta tarjeta"><a href = "#" class = "btn btn-primary listBtn"> Añadir Tarjeta</a></div></div>`;
+  <div class="card-body">
+  <h5 class="card-title">${titleList}</h5>
+  <input type="text" class="form-control" id="cardPlus" placeholder="Introduzca un título para esta tarjeta"><a href = "#" class = "btn btn-primary listBtn" onclick="listOfCards();getListOfHomeworks()"> Añadir Tarjeta</a></div></div>`;
 }
+
+function listOfCards() {
+  titleCard = document.getElementById('cardPlus').value;
+  homeworkDiv.innerHTML = `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+    <h5 class="card-title">${titleList}</h5><ul class="list-group list-group-flush">
+    <li class="list-group-item">${listingCards}</li>
+  </div><input type="text" class="form-control" id="cardPlus" placeholder="Introduzca un título para esta tarjeta"><a href = "#" class = "btn btn-primary listBtn" onclick="getListOfHomeworks();cardCreator()"; > Añadir Tarjeta</a></div></div> `;
+}
+
+
+
+// function drawCards() {
+
+//   homeworkDiv.style.display = 'none';
+//   listWithCardsContent.style.display = 'block';
+//   listWithCardsContent.innerHTML = `<div class="card" style="width: 18rem;">
+//     <div class="card-body">
+//     <h5 class="card-title">${titleList}</h5><ul class="list-group list-group-flush">
+//     <li class="list-group-item">${listingCards}</li>
+//   </div><input type="text" class="form-control" id="cardPlus" placeholder="Introduzca un título para esta tarjeta"><a href = "#" class = "btn btn-primary listBtn" onclick="getListOfHomeworks();drawCards())"> Añadir Tarjeta</a > </div></div > `;
+// }
--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,11 @@
+// function listCreator(titleList) {
+//   let listingTheLists = [];
+//   nameList.forEach((list) => {
+//     listingTheLists += list;
+//   })
+// }
+
+
 function saveNameList(nameList) {
   nameList = [];
   nameList.push(titleList);
@@ -5,6 +13,17 @@ function saveNameList(nameList) {
 
 }
 
-function getListOfHomeworks(homework) {
-
+function getListOfHomeworks(cardList) {
+  cardList = [];
+  cardList.push(titleCard);
+  console.log(cardList);
 }
+
+function cardCreator(listingCards) {
+  let listingCards = [];
+  cardList.forEach((card) => {
+    listingCards += card;
+  })
+  return listingCards;
+  console.log(listingCards);
+};
